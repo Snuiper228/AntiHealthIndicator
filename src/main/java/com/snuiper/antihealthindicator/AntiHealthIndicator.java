04e43fc634d267e7daebb68b0bdd131f4df6ac76
BuildTools <unconfigured@null.spigotmc.org>
2023-08-06 00:40:49 +0200

    Initial commit

---
+++ src/main/java/com/snuiper/antihealthindicator/AntiHealthIndicator.java
@@ -0,0 +1,200 @@
+package com.snuiper.antihealthindicator;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.StructureModifier;
+import com.comphenix.protocol.wrappers.WrappedAttribute;
+import com.comphenix.protocol.wrappers.WrappedDataValue;
+import com.comphenix.protocol.wrappers.WrappedWatchableObject;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.event.vehicle.VehicleExitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+
+public final class AntiHealthIndicator extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        MinecraftVersion minecraftVersion = MinecraftVersion.getServerVersion();
+        int serverVersion = minecraftVersion.getVersion();
+
+        Field modifierFiled;
+        try {
+            modifierFiled = WrappedAttribute.class.getDeclaredField("modifier");
+        } catch (NoSuchFieldException e) {
+            System.err.println("--------------------------------------");
+            System.err.println("|| Anti health indicator bug!!!");
+            System.err.println("|| Getting modifier field");
+            System.err.println("|| Exception: " + e);
+            System.err.println("--------------------------------------");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        modifierFiled.setAccessible(true);
+
+        getServer().getPluginManager().registerEvents(this, this);
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_METADATA, PacketType.Play.Server.UPDATE_ATTRIBUTES, PacketType.Play.Server.ENTITY_EFFECT) {
+            private final HashMap<Entity, Object> attributes = new HashMap<>();
+            private final HashMap<Entity, Double> baseValue = new HashMap<>();
+
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                if (event.isPlayerTemporary()) return;
+                PacketContainer packet = event.getPacket();
+                Player player = event.getPlayer();
+                int entityId = packet.getIntegers().read(0);
+                if (player.getEntityId() == entityId) {
+                    if (packet.getType() == PacketType.Play.Server.UPDATE_ATTRIBUTES) {
+                        Object value = baseValue.getOrDefault(player, null);
+                        Object list = attributes.getOrDefault(player, null);
+                        for (WrappedAttribute attribute : packet.getAttributeCollectionModifier().read(0)) {
+                            if (attribute.getAttributeKey().equals("generic.max_health") || attribute.getAttributeKey().equals("generic.maxHealth")) {
+                                try {
+                                    StructureModifier<Object> modifier = (StructureModifier<Object>) modifierFiled.get(attribute);
+                                    if (value != null) {
+                                        baseValue.remove(player);
+                                        modifier.withType(double.class).write(0, value);
+                                    }
+                                    if (list != null) {
+                                        attributes.remove(player);
+                                        modifier.withType(Collection.class).write(0, list);
+                                    }
+                                } catch (IllegalAccessException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                    return;
+                }
+                Entity entity = null;
+                for (Entity e : player.getWorld().getEntities()) {
+                    if (e.getEntityId() == entityId) {
+                        entity = e;
+                        break;
+                    }
+                }
+                if (!(entity instanceof LivingEntity)
+                        || entity instanceof EnderDragon || entity instanceof Wither
+                        || player.getVehicle() == entity) {
+                    return;
+                }
+
+                if (packet.getType() == PacketType.Play.Server.ENTITY_METADATA) {
+                    packet = packet.deepClone();
+                    event.setPacket(packet);
+                    if (serverVersion > MinecraftVersion.m1_19_3.getVersion()) {
+                        for (WrappedDataValue watchable : packet.getDataValueCollectionModifier().read(0)) {
+                            if (!(watchable.getValue() instanceof Float) || (float) watchable.getValue() <= 0.01D) {
+                                continue;
+                            }
+                            if (watchable.getIndex() == 9) { //Health
+                                if (entity instanceof IronGolem) {
+                                    IronGolem ironGolem = (IronGolem) entity;
+                                    if (ironGolem.getHealth() >= 75) {
+                                        watchable.setValue(75f);
+                                    } else if (ironGolem.getHealth() >= 50) {
+                                        watchable.setValue(50f);
+                                    } else if (ironGolem.getHealth() >= 25) {
+                                        watchable.setValue(25f);
+                                    } else {
+                                        watchable.setValue(1f);
+                                    }
+                                } else {
+                                    watchable.setValue(1f);
+                                }
+                            } else if (watchable.getIndex() == 15) { //Additional Hearts (AKA golden hearts)
+                                watchable.setValue(0f);
+                            }
+                        }
+                    } else {
+                        int healthIndex = 9;
+                        int additionalHeartsIndex = 15;
+
+                        if (serverVersion <= MinecraftVersion.m1_9_4.getVersion()) {
+                            healthIndex = 6;
+                            additionalHeartsIndex = 10;
+                        }else if (serverVersion <= MinecraftVersion.m1_16_5.getVersion()) {
+                            healthIndex = 8;
+                            additionalHeartsIndex = 14;
+                        }
+
+                        for (WrappedWatchableObject watchable : packet.getWatchableCollectionModifier().read(0)) {
+                            if (!(watchable.getValue() instanceof Float) || (float) watchable.getValue() <= 0.01D) {
+                                continue;
+                            }
+                            if (watchable.getIndex() == healthIndex) { //Health
+                                if (entity instanceof IronGolem) {
+                                    IronGolem ironGolem = (IronGolem) entity;
+                                    if (ironGolem.getHealth() >= 75) {
+                                        watchable.setValue(75f);
+                                    } else if (ironGolem.getHealth() >= 50) {
+                                        watchable.setValue(50f);
+                                    } else if (ironGolem.getHealth() >= 25) {
+                                        watchable.setValue(25f);
+                                    } else {
+                                        watchable.setValue(1f);
+                                    }
+                                } else {
+                                    watchable.setValue(1f);
+                                }
+                            } else if (watchable.getIndex() == additionalHeartsIndex) { //Additional Hearts (AKA golden hearts)
+                                watchable.setValue(0f);
+                            }
+                        }
+                    }
+                } else if (packet.getType() == PacketType.Play.Server.UPDATE_ATTRIBUTES) {
+                    for (WrappedAttribute attribute : packet.getAttributeCollectionModifier().read(0)) {
+                        if (attribute.getAttributeKey().equals("generic.max_health") || attribute.getAttributeKey().equals("generic.maxHealth")) {
+                            try {
+                                StructureModifier<Object> modifier = (StructureModifier<Object>) modifierFiled.get(attribute);
+                                baseValue.put(entity, (Double) modifier.withType(double.class).read(0));
+                                modifier.withType(double.class).write(0, 20d);
+                                attributes.put(entity, modifier.withType(Collection.class).read(0));
+                                modifier.withType(Collection.class).write(0, Collections.EMPTY_LIST);
+                            } catch (IllegalAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    @EventHandler
+    public void onMount(VehicleEnterEvent event) {
+        if (!(event.getEntered() instanceof Player)) {
+            return;
+        }
+        Vehicle vehicle = event.getVehicle();
+        Player player = (Player) event.getEntered();
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
+            if (!vehicle.isValid() || !player.isValid() || player.getVehicle() != vehicle) {
+                return;
+            }
+            ProtocolLibrary.getProtocolManager().updateEntity(vehicle, Collections.singletonList(player));
+        });
+    }
+
+    @EventHandler
+    public void onDismount(VehicleExitEvent event) {
+        if (!(event.getExited() instanceof Player)) {
+            return;
+        }
+        Vehicle vehicle = event.getVehicle();
+        Player player = (Player) event.getExited();
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> ProtocolLibrary.getProtocolManager().updateEntity(vehicle, Collections.singletonList(player)));
+    }
+}
---
+++ src/main/java/com/snuiper/antihealthindicator/MinecraftVersion.java
@@ -0,0 +1,229 @@
+package com.snuiper.antihealthindicator;
+
+import org.bukkit.Bukkit;
+
+public enum MinecraftVersion {
+    m1_9_0(1, 9, 0),
+    m1_9_2(1, 9, 2),
+    m1_9_4(1, 9, 4),
+    m1_10_2(1, 10, 2),
+    m1_11_0(1, 11, 0),
+    m1_11_1(1, 11, 1),
+    m1_11_2(1, 11, 2),
+    m1_12_0(1, 12, 0),
+    m1_12_1(1, 12, 1),
+    m1_12_2(1, 12, 2),
+    m1_13_0(1, 13, 0),
+    m1_13_1(1, 13, 1),
+    m1_13_2(1, 13, 2),
+    m1_14_0(1, 14, 0),
+    m1_14_1(1, 14, 1),
+    m1_14_2(1, 14, 2),
+    m1_14_3(1, 14, 3),
+    m1_14_4(1, 14, 4),
+    m1_15_0(1, 15, 0),
+    m1_15_1(1, 15, 1),
+    m1_15_2(1, 15, 2),
+    m1_16_1(1, 16, 1),
+    m1_16_2(1, 16, 2),
+    m1_16_3(1, 16, 3),
+    m1_16_4(1, 16, 4),
+    m1_16_5(1, 16, 5),
+    m1_17_0(1, 17, 0),
+    m1_17_1(1, 17, 1),
+    m1_18_0(1, 18, 0),
+    m1_18_1(1, 18, 1),
+    m1_18_2(1, 18, 2),
+    m1_19_0(1, 19, 0),
+    m1_19_1(1, 19, 1),
+    m1_19_2(1, 19, 2),
+    m1_19_3(1, 19, 3),
+    m1_19_4(1, 19, 4);
+
+    private static MinecraftVersion serverVersion;
+
+    public static MinecraftVersion getServerVersion() {
+        if (serverVersion == null) {
+            String minecraftVersionString = Bukkit.getBukkitVersion();
+            String[] minecraftVersionNumbers = minecraftVersionString.substring(0, minecraftVersionString.indexOf('-')).split("\\.");
+            String major;
+            String minor;
+            String build;
+            major = minecraftVersionNumbers[0];
+            minor = minecraftVersionNumbers[1];
+            if (minecraftVersionNumbers.length > 2) {
+                build = minecraftVersionNumbers[2];
+            } else {
+                build = "0";
+            }
+            try {
+                serverVersion = MinecraftVersion.valueOf("m" + major + "_" + minor + "_" + build);
+            } catch (IllegalArgumentException e) {
+                System.err.println("--------------------------------------");
+                System.err.println("|| Anti health indicator bug!!!");
+                System.err.println("|| Parsing minecraft version");
+                System.err.println("|| major: " + major);
+                System.err.println("|| minor: " + minor);
+                System.err.println("|| build: " + build);
+                System.err.println("|| Exception: " + e);
+                System.err.println("--------------------------------------");
+            }
+        }
+        return serverVersion;
+    }
+
+    public static String getMinecraftServerPackage(MinecraftVersion version) {
+        switch (version) {
+            case m1_9_0:
+            case m1_9_2:
+            case m1_9_4:
+            case m1_10_2:
+            case m1_11_0:
+            case m1_11_1:
+            case m1_11_2:
+            case m1_12_0:
+            case m1_12_1:
+            case m1_12_2:
+            case m1_13_0:
+            case m1_13_1:
+            case m1_13_2:
+            case m1_14_0:
+            case m1_14_1:
+            case m1_14_2:
+            case m1_14_3:
+            case m1_14_4:
+            case m1_15_0:
+            case m1_15_1:
+            case m1_15_2:
+            case m1_16_1:
+            case m1_16_2:
+            case m1_16_3:
+            case m1_16_4:
+            case m1_16_5:
+                return "net.minecraft.server.v" + version.major + "_" + version.minor + "_" + getRNumber(version);
+            case m1_17_0:
+            case m1_17_1:
+            case m1_18_0:
+            case m1_18_1:
+            case m1_18_2:
+            case m1_19_0:
+            case m1_19_1:
+            case m1_19_2:
+            case m1_19_3:
+            case m1_19_4:
+                return "net.minecraft";
+            default:
+                return null;
+        }
+    }
+
+    public static String getBukkitPackage(MinecraftVersion version) {
+        switch (version) {
+            case m1_9_0:
+            case m1_9_2:
+            case m1_9_4:
+            case m1_10_2:
+            case m1_11_0:
+            case m1_11_1:
+            case m1_11_2:
+            case m1_12_0:
+            case m1_12_1:
+            case m1_12_2:
+            case m1_13_0:
+            case m1_13_1:
+            case m1_13_2:
+            case m1_14_0:
+            case m1_14_1:
+            case m1_14_2:
+            case m1_14_3:
+            case m1_14_4:
+            case m1_15_0:
+            case m1_15_1:
+            case m1_15_2:
+            case m1_16_1:
+            case m1_16_2:
+            case m1_16_3:
+            case m1_16_4:
+            case m1_16_5:
+            case m1_17_0:
+            case m1_17_1:
+            case m1_18_0:
+            case m1_18_1:
+            case m1_18_2:
+            case m1_19_0:
+            case m1_19_1:
+            case m1_19_2:
+            case m1_19_3:
+            case m1_19_4:
+                return "org.bukkit.craftbukkit.v" + version.major + "_" + version.minor + "_" + getRNumber(version);
+            default:
+                return null;
+        }
+    }
+
+    private static String getRNumber(MinecraftVersion version) {
+        switch (version) {
+            case m1_9_0:
+            case m1_9_2:
+            case m1_10_2:
+            case m1_11_0:
+            case m1_11_1:
+            case m1_11_2:
+            case m1_12_0:
+            case m1_12_1:
+            case m1_12_2:
+            case m1_13_0:
+            case m1_14_0:
+            case m1_14_1:
+            case m1_14_2:
+            case m1_14_3:
+            case m1_14_4:
+            case m1_15_0:
+            case m1_15_1:
+            case m1_15_2:
+            case m1_16_1:
+            case m1_17_0:
+            case m1_17_1:
+            case m1_18_0:
+            case m1_18_1:
+            case m1_19_0:
+            case m1_19_1:
+            case m1_19_2:
+                return "R1";
+            case m1_9_4:
+            case m1_13_1:
+            case m1_13_2:
+            case m1_16_2:
+            case m1_16_3:
+            case m1_18_2:
+            case m1_19_3:
+                return "R2";
+            case m1_16_4:
+            case m1_16_5:
+            case m1_19_4:
+                return "R3";
+            default:
+                return null;
+        }
+    }
+
+    private final int major;
+    private final int minor;
+    private final int build;
+    private final int version;
+
+    MinecraftVersion(int major, int minor, int build) {
+        this.major = major;
+        this.minor = minor;
+        this.build = build;
+        this.version = Integer.parseInt("" + major + minor + build);
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public String getVersionString() {
+        return major + "." + minor + "." + build;
+    }
+}
